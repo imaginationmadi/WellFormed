674315d5d71ea7aea1058bd513aaf4f7d5f49eeb
imaginationmadi <145612004+imaginationmadi@users.noreply.github.com>
2023-09-30 15:10:24 -0700

    WellFormed
    Prints a well informing 2 paragraphs on well formed programs and print.ln statements

---
+++ WellFormed.java
@@ -0,0 +1,17 @@
+// chapter 1 wellformed
+
+public class WellFormed {
+
+	public static void main (String args[]) {
+		
+		System.out.println ("A well formed Java Program has");
+		System.out.println ("a main method with { and }");
+		System.out.println ("braces.");
+		System.out.println ();
+		System.out.println ("A System.out.println statement");
+		System.out.println ("has ( and ) and usually a \nString that starts and ends \n with a \" character.");
+		System.out.println (" (but we type \\\" instead!)");
+		
+	}
+
+}
